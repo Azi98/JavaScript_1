bdeea52f38c69357497e4c4e69ca057cb02914b2
Aznaur <bedraev98@gmail.com>
2023-02-25 10:53:01 +0100

    1. practice

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 "use strict"
 
-console.log(4 + "5")
+const numberOfFilms = +prompt('Сколько фильмов ты посмотрел?', 'Тебя ебет?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const rateOfFilms = prompt("Один из последних просмотренных фильмов?", ""),
+    answer = +prompt("На сколько его оцените", "");
+
+const rateOfFilms1 = prompt("Один из последних просмотренных фильмов?", ""),
+    answer1 = +prompt("На сколько его оцените", "");
+
+personalMovieDB.movies[rateOfFilms] = answer;
+personalMovieDB.movies[rateOfFilms1] = answer1;
+
+console.log(personalMovieDB.movies);
+
 
